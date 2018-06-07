a8d34c96146ff50304a80c8aeb417daa9c76b8f6
Krzysztof Syska <ksyska@pl.sii.eu>
2018-06-07 16:45:11 +0200

    added h2 access to url and fixed Winner entity

--- src/main/java/org/prezydium/streets/model/Winner.java
+++ src/main/java/org/prezydium/streets/model/Winner.java
@@ -1,12 +1,15 @@
 package org.prezydium.streets.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Winner {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nick;
