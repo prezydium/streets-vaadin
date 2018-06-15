e2879c301dc792049340ed50eadec6eaa40275b4
Krzysztof Syska <ksyska@pl.sii.eu>
2018-06-15 12:11:34 +0200

    working h2 database

--- src/main/java/org/prezydium/streets/jpa/WinnersRepository.java
+++ src/main/java/org/prezydium/streets/jpa/WinnersRepository.java
@@ -3,6 +3,8 @@ package org.prezydium.streets.jpa;
 
 import org.prezydium.streets.model.Winner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WinnersRepository extends JpaRepository<Winner, Long> {
 }
